8765e4fa2d474413f7c4ebd678fd69be8cc43cab
YashMunjal <yashmu45@gmail.com>
2020-03-10 02:44:18 +0530

    Updates

---
+++ precache-manifest.7fb88403ce949a5191b4368cec8427d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94fc30ca9d71b670e0b1bd5fd7712fd2",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "d6f2505f380a3ddff742",
+    "url": "/recipe-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cf55bf7a6c9ef363847c",
+    "url": "/recipe-app/static/js/2.b0ecaf07.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/recipe-app/static/js/2.b0ecaf07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6f2505f380a3ddff742",
+    "url": "/recipe-app/static/js/main.0c705a02.chunk.js"
+  },
+  {
+    "revision": "887dbc60425b31b5d4ea",
+    "url": "/recipe-app/static/js/runtime-main.2006017e.js"
+  }
+]);
